1fbea1092645c6b7b6101e6da68848b71d8cd555
Uzma-Azam21 <uzmaazam241@gmail.com>
2024-03-26 08:03:21 -0700

    Add files

---
+++ main.js
@@ -0,0 +1,38 @@
+// Student Name: Uzma Azam
+// Registration no: 00330608
+// Day Tuesday (9:00 am)
+// Class Assignment _ 4
+// Types and Interface in Objects
+// Example_ 1;
+// Define the companyType type
+var Industry = ["Tech company"];
+;
+// Company data
+var Company = {
+    name: "NETSOL Technologies Pakistan",
+    industry: "Information Technology & Services",
+    foundedYear: 1996,
+    isPublic: true,
+    products: ["NFS", "NFS Ascent", "NFS Digital"],
+    address: {
+        streetName: "Block 3, P.E.C.H.S",
+        city: "Karachi",
+        country: "Pakistan",
+        zipCode: "75400",
+    },
+};
+console.log(Company);
+//  Product data
+var product = {
+    name: "Laptop",
+    price: 186999,
+    brand: "Dell",
+    category: "Electronics",
+    inStock: true,
+    dimensions: {
+        length: 13,
+        width: 9,
+        height: 0.75
+    },
+};
+console.log(product);
---
+++ main.ts
@@ -0,0 +1,77 @@
+// Student Name: Uzma Azam
+// Registration no: 00330608
+// Day Tuesday (9:00 am)
+// Class Assignment _ 4
+// Types and Interface in Objects
+
+
+// Example_ 1;
+// Define the companyType type
+
+let Industry = ["Tech company"]
+type companyType = {
+  name: string;
+  industry: string;
+  foundedYear: Number;
+  isPublic: Boolean;
+  products: string[];
+  address: AddressType
+
+};
+// Define the Interface
+interface AddressType {
+  streetName: string;
+  city: string;
+  country: string;
+  zipCode: string;
+};
+
+// Company data
+let Company: companyType = {
+  name: "NETSOL Technologies Pakistan",
+  industry: "Information Technology & Services",
+  foundedYear: 1996,
+  isPublic: true,
+  products: ["NFS", "NFS Ascent", "NFS Digital"],
+  address: {
+    streetName: "Block 3, P.E.C.H.S",
+    city: "Karachi",
+    country: "Pakistan",
+    zipCode: "75400",
+  },
+};
+console.log(Company);
+
+// Example_ 2;
+// Define the Dimensions interface
+interface Dimensions {
+  length: number;
+  width: number;
+  height: number;
+}
+
+// Define the productType type
+type productType = {
+  name: string;
+  price: number;
+  brand: string;
+  category: string;
+  inStock: boolean;
+  dimensions: Dimensions;
+};
+
+//  Product data
+const product: productType = {
+  name: "Laptop",
+  price: 186999,
+  brand: "Dell",
+  category: "Electronics",
+  inStock: true,
+  dimensions: {
+    length: 13,
+    width: 9,
+    height: 0.75
+  },
+
+};
+console.log(product);
